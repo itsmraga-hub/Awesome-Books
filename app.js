e7b2108db2c870a522b35a1823dbac3704a3c367
Raga <ragawilliam570@gmail.com>
2022-08-01 17:57:27 +0300

    Added javascript logic to add books to collection

--- app.js
+++ app.js
@@ -1,44 +1,72 @@
 const bookContainer = document.querySelector('.books-container');
-const form = document.querySelector('.form');
+const booksArr = document.querySelectorAll('.books');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
 const addBtn = document.querySelector('.add-btn');
 const removeBtns = document.querySelectorAll('.btn');
+let i = 0
 
-const books = [
-  {
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum'
-  },
-  {
-    title: 'Lorem Ipsum',
-    author: 'Lorem Ipsum'
-  }
-]
+let books = [];
+let myLibrary = [];
 
-function displayBook(i) {
+function showBooks(i) {
   const div = document.createElement('div');
   div.className = 'book';
-  div.innerHTML = `<p>${books[i].title}</p>
-  <p>${books[i].author}</p>
+  div.id = `book${i}`;
+  div.innerHTML = `<p>${myLibrary[i].title}</p>
+  <p>${myLibrary[i].author}</p>
   <button class="btn">Remove</button>`
   bookContainer.appendChild(div);
-};
+}
+
+const getBooksFromStorage = () => {
+  let localData = localStorage.getItem('book');
+  if (localData) {
+    myLibrary = JSON.parse(localData);
+  }
+}
+
+getBooksFromStorage();
+
+for (i = 0; i < myLibrary.length; i += 1) {
+  showBooks(i);
+}
 
 function createBook(bookTitle, bookAuthor) {
   let book = {
     title: bookTitle,
     author: bookAuthor
   }
-  books.push(book); 
+  return book;
 };
 
+function add(btitle, bauthor) {
+  let b = createBook(btitle, bauthor);
+  myLibrary.push(b);
+}
+
 // Event listener for add button
-addBtn.addEventListener('click', () => {
-  let bookT = form[0].value;
-  let bookA = form[1].value;
-  createBook(bookT, bookA);
-  for (let i = 0; i < books.length; i += 1) {
-    displayBook(i);
-  }
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let bookT = title.value;
+  let bookA = author.value;
+  add(bookT, bookA);
+  localStorage.setItem('book', JSON.stringify(myLibrary));
+  showBooks(myLibrary.length - 1);
+  title.value = '';
+  author.value = '';
 });
 
-// Local storage
+// Store the inputs to Local storage
+let inputs = [title, author];
+inputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    let bTitle = title.value;
+    let bAuthor = author.value;
+    let newBook = {
+      title: bTitle,
+      author: bAuthor
+    }
+    localStorage.setItem('newBook', JSON.stringify(newBook));
+  })
+})
