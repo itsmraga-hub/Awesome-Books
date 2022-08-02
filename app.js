dee7c3847323e6f318e726c9d018d5df100004f3
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-08-02 11:45:14 +0100

    added the class refactoring of the awesome book in app.js

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const addBtn = document.querySelector('.add-btn');
 let myLibrary = [];
 const books = [];
 
+// Refactor
+class Book{
+  constructor(bookTitle,bookAuthor){
+    this.title = bookTitle;
+    this.author = bookAuthor;
+  }
+   SetBook() {
+
+   books.push(this);
+   showBooks(this);
+   setTimeout(window.location.reload(), 2000);
+   localStorage.setItem('book', JSON.stringify(books));
+   title.value = '';
+   author.value = '';
+  }
+  removeBook(books,index){
+    books.splice(index, 1);
+  localStorage.setItem('book', JSON.stringify(books));
+  setTimeout(window.location.reload(), 2000);
+  }
+}
+console.log(new Book('novel','soul'));
+
+
+//End Here
 function showBooks(b) {
   const div = document.createElement('div');
   div.className = 'book';
@@ -25,43 +50,49 @@ const getBooksFromStorage = () => {
   }
 };
 
-function createBook(bookTitle, bookAuthor) {
-  const book = {
-    title: bookTitle,
-    author: bookAuthor,
-  };
-  return book;
-}
+// function createBook(bookTitle, bookAuthor) {
+//   const book = {
+//     title: bookTitle,
+//     author: bookAuthor,
+//   };
+//   return book;
+// }
 
-function add() {
-  const bookT = title.value;
-  const bookA = author.value;
-  const b = createBook(bookT, bookA);
-  books.push(b);
-  showBooks(b);
-  setTimeout(window.location.reload(), 2000);
-  localStorage.setItem('book', JSON.stringify(books));
-  title.value = '';
-  author.value = '';
-}
+// function add() {
+//   const bookT = title.value;
+//   const bookA = author.value;
+//   const b = createBook(bookT, bookA);
+//   books.push(b);
+//   showBooks(b);
+  // setTimeout(window.location.reload(), 2000);
+  // localStorage.setItem('book', JSON.stringify(books));
+  // title.value = '';
+  // author.value = '';
+// }
 
 // Event listener for add button
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  add();
+  // add();
+  let bk = new Book(title.value,author.value);
+  bk.SetBook();
 });
 
 getBooksFromStorage();
 
-function remove(books, i) {
-  books.splice(i, 1);
-  localStorage.setItem('book', JSON.stringify(books));
-  setTimeout(window.location.reload(), 2000);
-}
+// function remove(books, i) {
+//   // books.splice(i, 1);
+//   // localStorage.setItem('book', JSON.stringify(books));
+//   // setTimeout(window.location.reload(), 2000);
+// }
 
 const removeBtns = document.querySelectorAll('.btnRemove');
 removeBtns.forEach((removeBtn, i) => {
   removeBtn.addEventListener('click', () => {
-    remove(books, i);
+    // remove(books, i);
+    let bk= new Book();
+        bk.removeBook(books,i);
   });
 });
+
+
